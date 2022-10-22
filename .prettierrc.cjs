2dc361a99568bcdd7646070a735eb68a370c8676
Mukitul Islam Mukit <hellomukit@gmail.com>
2022-10-23 02:09:21 +0600

    chore: update prettier config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: [require('prettier-plugin-tailwindcss')],
-}
+  plugins: [require("prettier-plugin-tailwindcss")],
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+};
