3a0c6cc0425c086b7bb80e0ae1bb00cf96b34272
Mukitul Islam Mukit <hellomukit@gmail.com>
2022-10-22 12:56:25 +0600

    feat: add faq component

---
+++ src/components/home/FAQ.tsx
@@ -0,0 +1,60 @@
+const faqs = [
+  {
+    question: "How do you make holy water?",
+    answer:
+      "You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+  {
+    question: "How do you make holy water?",
+    answer:
+      "You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+  {
+    question: "How do you make holy water?",
+    answer:
+      "You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+  {
+    question: "How do you make holy water?",
+    answer:
+      "You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+];
+
+export const FAQ = () => {
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:py-20 lg:px-8">
+        <div className="lg:grid lg:grid-cols-3 lg:gap-8">
+          <div>
+            <h2 className="text-3xl font-extrabold text-gray-900">
+              Frequently asked questions
+            </h2>
+            <p className="mt-4 text-lg text-gray-500">
+              Can’t find the answer you’re looking for? Reach out to our{" "}
+              <a
+                href="#"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                customer support
+              </a>{" "}
+              team.
+            </p>
+          </div>
+          <div className="mt-12 lg:col-span-2 lg:mt-0">
+            <dl className="space-y-12">
+              {faqs.map((faq) => (
+                <div key={faq.question}>
+                  <dt className="text-lg font-medium leading-6 text-gray-900">
+                    {faq.question}
+                  </dt>
+                  <dd className="mt-2 text-base text-gray-500">{faq.answer}</dd>
+                </div>
+              ))}
+            </dl>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
