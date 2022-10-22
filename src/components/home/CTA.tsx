5fe11f314f2abdb6f678f39ba8e8a879c7e78e5e
Mukitul Islam Mukit <hellomukit@gmail.com>
2022-10-22 12:56:41 +0600

    feat: add call to action component

---
+++ src/components/home/CTA.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const CTA = () => {
+  return (
+    <div className="bg-indigo-700">
+      <div className="mx-auto max-w-2xl py-16 px-4 text-center sm:py-20 sm:px-6 lg:px-8">
+        <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
+          <span className="block">Boost your productivity.</span>
+          <span className="block">Start using Workflow today.</span>
+        </h2>
+        <p className="mt-4 text-lg leading-6 text-indigo-200">
+          Ac euismod vel sit maecenas id pellentesque eu sed consectetur.
+          Malesuada adipiscing sagittis vel nulla nec.
+        </p>
+        <a
+          href="#"
+          className="mt-8 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600 hover:bg-indigo-50 sm:w-auto"
+        >
+          Sign up for free
+        </a>
+      </div>
+    </div>
+  );
+};
