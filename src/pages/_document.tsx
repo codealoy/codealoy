9242e62945ddc26964a7e102a7bca911e5e41133
Mukitul Islam Mukit <hellomukit@gmail.com>
2022-10-23 02:13:14 +0600

    fix: update favicon on site header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import { COLORS } from "../constants/site-settings";
 
 const themeScript = `
   let mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
@@ -55,12 +56,42 @@ export default function Document() {
   return (
     <Html
       className="scroll-smooth antialiased [font-feature-settings:'ss01']"
-      lang="en"
+      lang="bn"
     >
       <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/static/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/static/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/static/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/static/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/static/favicon/safari-pinned-tab.svg"
+          color={COLORS.PRIMARY}
+        />
+        <link rel="shortcut icon" href="/static/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content={COLORS.PRIMARY} />
+        <meta
+          name="msapplication-config"
+          content="/static/favicon/browserconfig.xml"
+        />
+        <meta name="theme-color" content={COLORS.PRIMARY} />
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
       </Head>
-      <body className="bg-white dark:bg-slate-900">
+      <body className="bg-white antialiased dark:bg-slate-900">
         <Main />
         <NextScript />
       </body>
