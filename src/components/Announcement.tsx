066442dc01544659b1e3a7487f00de0ea0c419e0
Mukitul Islam Mukit <hellomukit@gmail.com>
2022-10-22 12:54:12 +0600

    feat: add anouncement component

---
+++ src/components/Announcement.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { MegaphoneIcon, XMarkIcon } from "@heroicons/react/24/outline";
+
+export const Announcement: React.FC<{
+  hideAnnouncement: () => void;
+}> = ({ hideAnnouncement }) => {
+  return (
+    <div className="z-50 bg-indigo-600">
+      <div className="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
+        <div className="flex flex-wrap items-center justify-between">
+          <div className="flex w-0 flex-1 items-center">
+            <span className="flex rounded-lg bg-indigo-800 p-2">
+              <MegaphoneIcon
+                className="h-6 w-6 text-white"
+                aria-hidden="true"
+              />
+            </span>
+            <p className="ml-3 truncate font-medium text-white">
+              <span className="md:hidden">We announced a new product!</span>
+              <span className="hidden md:inline">
+                Big news! We&apos;re excited to announce a brand new product.
+              </span>
+            </p>
+          </div>
+          <div className="order-3 mt-2 w-full flex-shrink-0 sm:order-2 sm:mt-0 sm:w-auto">
+            <a
+              href="#"
+              className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium text-indigo-600 shadow-sm hover:bg-indigo-50"
+            >
+              Learn more
+            </a>
+          </div>
+          <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
+            <button
+              type="button"
+              className="-mr-1 flex rounded-md p-2 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
+              onClick={hideAnnouncement}
+            >
+              <span className="sr-only">Dismiss</span>
+              <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
