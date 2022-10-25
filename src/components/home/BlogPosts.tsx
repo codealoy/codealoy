29d48388eac658f11a60db500cb60f18e83103c5
alaminsahed <alaminsahed97@gmail.com>
2022-10-26 03:39:52 +0600

    feat: homepage dark version till blog

--- src/components/home/BlogPosts.tsx
+++ src/components/home/BlogPosts.tsx
@@ -57,21 +57,21 @@ const posts = [
 
 export const BlogPosts = () => {
   return (
-    <div className="relative bg-gray-50 px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28">
+    <div className="relative bg-gray-50 px-4 pt-16 pb-20 dark:border-t dark:border-slate-100/5 dark:bg-slate-900 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28">
       <div className="absolute inset-0">
-        <div className="h-1/3 bg-white sm:h-2/3" />
+        <div className="h-1/3 bg-white dark:bg-slate-900 sm:h-2/3" />
       </div>
       <div className="relative mx-auto max-w-7xl">
         <div className="text-center">
-          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
             From the blog
           </h2>
-          <p className="mx-auto mt-3 max-w-2xl text-xl text-gray-500 sm:mt-4">
+          <p className="mx-auto mt-3 max-w-2xl text-xl text-gray-500 dark:text-slate-300 sm:mt-4">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa
             libero labore natus atque, ducimus sed.
           </p>
         </div>
-        <div className="mx-auto mt-12 grid max-w-lg gap-5 lg:max-w-none lg:grid-cols-3">
+        <div className="mx-auto mt-12 grid max-w-lg gap-5  lg:max-w-none lg:grid-cols-3">
           {posts.map((post) => (
             <div
               key={post.title}
@@ -84,7 +84,7 @@ export const BlogPosts = () => {
                   alt=""
                 />
               </div>
-              <div className="flex flex-1 flex-col justify-between bg-white p-6">
+              <div className="flex flex-1 flex-col justify-between bg-white p-6 dark:bg-slate-800">
                 <div className="flex-1">
                   <p className="text-sm font-medium text-indigo-600">
                     <a href={post.category.href} className="hover:underline">
