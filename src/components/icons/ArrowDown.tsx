051b44217ea9d607d434a30b98dbd72e268169a7
Mukitul Islam Mukit <hellomukit@gmail.com>
2022-10-30 20:50:33 +0600

    fix: remove fixed icon height & width

--- src/components/icons/ArrowDown.tsx
+++ src/components/icons/ArrowDown.tsx
@@ -8,7 +8,6 @@ const ArrowDown = () => {
       viewBox="0 0 24 24"
       stroke-width="1.5"
       stroke="currentColor"
-      className="h-6 w-6"
     >
       <path
         stroke-linecap="round"
