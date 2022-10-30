3ce6f08dd6795fae037387289d5b5987956f30d7
Mukitul Islam Mukit <hellomukit@gmail.com>
2022-10-31 02:31:22 +0600

    refactor: update primary nav items component name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ import { Search } from '../components/Search';
 import { ThemeSelector } from '../components/ThemeSelector';
 import { ThemeContext } from '../contexts/ThemeContext';
 import { GithubIcon } from './icons/GithubIcon';
-import { NavItems } from './NavItems';
+import { PrimaryNavItems } from './PrimaryNavItems';
 import { MobileNavbar } from './MobileNavbar';
 
 export const Header: React.FC<any> = () => {
@@ -53,7 +53,7 @@ export const Header: React.FC<any> = () => {
       >
         <nav className="text-sm font-semibold leading-6 text-slate-700 dark:text-slate-200">
           <ul className="flex space-x-10">
-            <NavItems />
+            <PrimaryNavItems />
           </ul>
         </nav>
         <div className="ml-6 flex items-center border-l border-slate-200 pl-6 dark:border-slate-800">
