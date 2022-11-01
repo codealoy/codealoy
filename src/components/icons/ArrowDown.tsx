57f017c3925d35760fea5d5f98ef064e177b43d3
Mukitul Islam Mukit <hellomukit@gmail.com>
2022-11-01 10:26:34 +0600

    fix: update svg attribute names to camelCase

--- src/components/icons/ArrowDown.tsx
+++ src/components/icons/ArrowDown.tsx
@@ -6,12 +6,12 @@ const ArrowDown = () => {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth={1.5}
       stroke="currentColor"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M19.5 8.25l-7.5 7.5-7.5-7.5"
       />
     </svg>
