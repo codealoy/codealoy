f831bcdceefdc66d86c5b184cd1bb437198f7d25
alaminsahed <alaminsahed97@gmail.com>
2022-11-02 11:27:11 +0600

    feat: add mobile bar structure

--- src/layouts/CourseLayout.tsx
+++ src/layouts/CourseLayout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import clsx from 'clsx';
 import Link from 'next/link';
 
@@ -10,7 +10,9 @@ import { getTableOfContent } from '../utils/getTableOfContent';
 import { useTableOfContents } from '../hooks/useTableOfContent';
 import { Tag } from '../components/Tag';
 import { NavigationItem } from '../constants/navigations';
-import { useRouter } from 'next/router';
+import { Router, useRouter } from 'next/router';
+import MobileCourseNav from 'src/components/MobileCourseNav';
+import { Dialog } from '@headlessui/react';
 
 interface CourseLayoutProps extends MarkdocNextJsPageProps {
   navigationItems: NavigationItem[];
@@ -32,9 +34,22 @@ export const CourseLayout: React.FC<CourseLayoutProps> = ({
     illustrations,
   } = markdoc?.frontmatter!;
 
+  const [navIsOpen, setNavIsOpen] = useState(false);
+
   const tableOfContents = getTableOfContent({ markdoc });
 
   const router = useRouter();
+
+  useEffect(() => {
+    if (!navIsOpen) return;
+    function handleRouteChange() {
+      setNavIsOpen(false);
+    }
+    Router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [navIsOpen]);
 
   const allLinks = navigationItems.flatMap((section) => section.links);
   const linkIndex = allLinks.findIndex(
@@ -62,6 +77,82 @@ export const CourseLayout: React.FC<CourseLayoutProps> = ({
   return (
     <>
       <Header />
+
+      <div className=" flex items-center border-b border-slate-900/10 p-4 dark:border-slate-50/[0.06] lg:hidden">
+        <button
+          type="button"
+          onClick={() => setNavIsOpen(!navIsOpen)}
+          className="text-slate-500 hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300"
+        >
+          <span className="sr-only">Navigation</span>
+          <svg width="24" height="24">
+            <path
+              d="M5 6h14M5 12h14M5 18h14"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+            />
+          </svg>
+        </button>
+        {/* <MobileCourseNav navigationItems={navigationItems} /> */}
+        {/* {navIsOpen && <Navigation navigationItems={navigationItems} />} */}
+        <Dialog
+          as="div"
+          open={navIsOpen}
+          onClose={() => setNavIsOpen(false)}
+          className="fixed inset-0 z-50 overflow-y-auto lg:hidden"
+        >
+          <Dialog.Overlay className="fixed inset-0 bg-black/20 backdrop-blur-sm dark:bg-slate-900/80" />
+          <div className="relative w-80 max-w-[calc(100%-3rem)] bg-white p-6 dark:bg-slate-800">
+            <button
+              type="button"
+              onClick={() => setNavIsOpen(false)}
+              className="absolute top-5 right-5 z-10 flex h-8 w-8 items-center justify-center text-slate-500 hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300"
+            >
+              <span className="sr-only">Close navigation</span>
+              <svg viewBox="0 0 10 10" className="h-2.5 w-2.5 overflow-visible">
+                <path
+                  d="M0 0L10 10M10 0L0 10"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                />
+              </svg>
+            </button>
+          </div>
+        </Dialog>
+        {navigationItems.map((section) => (
+          <>
+            {console.log({ section })}
+            <ol className="ml-4 flex min-w-0 whitespace-nowrap text-sm leading-6">
+              {section && (
+                <li className="flex items-center">
+                  {/* {title} */}
+                  <svg
+                    width="3"
+                    height="6"
+                    aria-hidden="true"
+                    className="mx-3 overflow-visible text-slate-400"
+                  >
+                    <path
+                      d="M0 0L3 3L0 6"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                    />
+                  </svg>
+                </li>
+              )}
+              <li className="truncate font-semibold text-slate-900 dark:text-slate-200">
+                {/* {section.title} */}
+              </li>
+            </ol>
+          </>
+        ))}
+      </div>
 
       <div className="relative mx-auto flex max-w-8xl justify-center sm:px-2 lg:px-8 xl:px-12">
         <div className="hidden lg:relative lg:block lg:flex-none">
