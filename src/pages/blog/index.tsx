854660fa6ec6d9634b2e0004448bb6377af40a26
Mukitul Islam Mukit <hellomukit@gmail.com>
2022-11-08 11:07:58 +0600

    style: fix dark mode for blog posts component

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -2,9 +2,9 @@ import { BlogPosts } from '../../components/home/BlogPosts';
 
 const BlogPage = () => {
   return (
-    <div className="relative mx-auto mb-20 min-h-screen max-w-8xl bg-white px-4 sm:px-6 lg:px-8 xl:px-12">
+    <div className="relative mx-auto mb-20 min-h-screen max-w-8xl px-4 sm:px-6 lg:px-8 xl:px-12">
       <div className="py-16 px-4 text-center sm:px-6 lg:px-8">
-        <h1 className="text-4xl font-extrabold tracking-tight text-slate-900">
+        <h1 className="text-4xl font-extrabold tracking-tight text-slate-700 dark:text-slate-300">
           Blog Posts
         </h1>
         <p className="mx-auto mt-4 max-w-xl text-base text-slate-500">
