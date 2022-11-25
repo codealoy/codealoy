bc3143db46d1ffa557e8fe9c00b36f2a38ca70dd
Mukitul Islam Mukit <hellomukit@gmail.com>
2022-11-25 23:47:50 +0600

    docs: update cta content

--- src/components/home/CTA.tsx
+++ src/components/home/CTA.tsx
@@ -5,18 +5,16 @@ export const CTA = () => {
     <div className="bg-indigo-700">
       <div className="mx-auto max-w-2xl py-16 px-4 text-center sm:py-20 sm:px-6 lg:px-8">
         <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
-          <span className="block">Boost your productivity.</span>
-          <span className="block">Start using Workflow today.</span>
+          <span className="block">আজ থেকেই শুরু হোক নতুন পথচলা</span>
         </h2>
         <p className="mt-4 text-lg leading-6 text-indigo-200">
-          Ac euismod vel sit maecenas id pellentesque eu sed consectetur.
-          Malesuada adipiscing sagittis vel nulla nec.
+          সফটওয়্যার ডেভেলপার হিসাবে ক্যারিয়ার গড়ে তোলার এখনই উপযুক্ত সময়
         </p>
         <a
           href="#"
           className="mt-8 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600 hover:bg-indigo-50 sm:w-auto"
         >
-          Sign up for free
+          শেখা শুরু করুন
         </a>
       </div>
     </div>
