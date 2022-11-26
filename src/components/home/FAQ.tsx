e62275fbff68a6ba2ca69f41578e56a82d15eaf5
Mukitul Islam Mukit <hellomukit@gmail.com>
2022-11-26 22:48:25 +0600

    feat: change faq section layout

--- src/components/home/FAQ.tsx
+++ src/components/home/FAQ.tsx
@@ -2,61 +2,54 @@ const faqs = [
   {
     question: 'How do you make holy water?',
     answer:
-      'You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.',
+      '1 You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.',
   },
   {
     question: 'How do you make holy water 2?',
     answer:
-      'You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.',
+      '2 You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.',
   },
   {
     question: 'How do you make holy water 3?',
     answer:
-      'You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.',
+      '3 You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.',
   },
   {
     question: 'How do you make holy water 4?',
     answer:
-      'You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.',
+      '4 You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.',
   },
 ];
 
 export const FAQ = () => {
   return (
-    <div className="dark:border-tdark:border-t dark:border-slate-100/5 dark:text-gray-500">
-      <div className="mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:py-20 lg:px-8">
-        <div className="lg:grid lg:grid-cols-3 lg:gap-8">
-          <div>
-            <h2 className="text-3xl font-extrabold text-gray-900 dark:text-white">
-              Frequently asked questions
-            </h2>
-            <p className="mt-4 text-lg text-gray-500">
-              Can’t find the answer you’re looking for? Reach out to our{' '}
-              <a
-                href="#"
-                className="font-medium text-indigo-600 hover:text-indigo-500"
-              >
-                customer support
-              </a>{' '}
-              team.
-            </p>
-          </div>
-          <div className="mt-12 lg:col-span-2 lg:mt-0">
-            <dl className="space-y-12">
-              {faqs.map((faq) => (
-                <div key={faq.question}>
-                  <dt className="text-lg font-medium leading-6 text-gray-900 dark:text-white">
-                    {faq.question}
-                  </dt>
-                  <dd className="mt-2 text-base text-gray-500 dark:text-slate-300">
-                    {faq.answer}
-                  </dd>
-                </div>
-              ))}
-            </dl>
-          </div>
+    <section className="bg-white dark:border-t dark:border-slate-100/5 dark:bg-slate-900">
+      <div className="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div className="max-w-2xl lg:mx-auto lg:text-center">
+          <h2 className="text-3xl font-extrabold tracking-tight text-slate-900 dark:text-slate-100 sm:text-4xl">
+            Frequently asked questions
+          </h2>
+          <p className="mt-4 text-slate-500 dark:text-slate-400">
+            Ac euismod vel sit maecenas id pellentesque eu sed consectetur.
+            Malesuada adipiscing sagittis vel nulla nec. Urna, sed a lectus
+            elementum blandit et.
+          </p>
+        </div>
+        <div className="mt-20">
+          <dl className="space-y-10 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:gap-y-10 lg:space-y-0">
+            {faqs.map((faq) => (
+              <div key={faq.question}>
+                <dt className="font-semibold text-slate-800 dark:text-slate-200">
+                  {faq.question}
+                </dt>
+                <dd className="mt-3 text-slate-500 dark:text-slate-400">
+                  {faq.answer}
+                </dd>
+              </div>
+            ))}
+          </dl>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
