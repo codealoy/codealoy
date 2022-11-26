91cb7a148b4d6e7b322fee6031e6bad7b3fc28bc
Mukitul Islam Mukit <hellomukit@gmail.com>
2022-11-27 01:05:00 +0600

    feat: update team section content

--- src/components/home/Team.tsx
+++ src/components/home/Team.tsx
@@ -1,26 +1,65 @@
-const people = [
+import Image from 'next/image';
+import { FaGithubSquare, FaLinkedin, FaFacebookSquare } from 'react-icons/fa';
+
+import imageOfMukit from '../../images/home/codealoy-team-mukit.png';
+import imageOfShahed from '../../images/home/codealoy-team-shahed.png';
+
+const teamMembers = [
   {
-    name: 'Whitney Francis',
-    role: 'Software Engineer',
-    imageUrl: 'https://i.pravatar.cc/1024?img=2',
-    twitterUrl: '#',
-    linkedinUrl: '#',
+    name: `মুকিতুল ইসলাম মুকিত`,
+    role: `প্রতিষ্ঠাতা এবং লিড মেইনটেইনার`,
+    position: `সিনিয়র সফটওয়্যার ইঞ্জিনিয়ার`,
+    image: imageOfMukit,
+    githubUrl: 'https://github.com/mimukit',
+    linkedinUrl: 'https://www.linkedin.com/in/mimukit/',
+    facebookUrl: 'https://www.facebook.com/mimukit',
   },
   {
-    name: 'John Doe',
-    role: 'Copywriter',
-    imageUrl: 'https://i.pravatar.cc/1024?img=4',
-    twitterUrl: '#',
-    linkedinUrl: '#',
-  },
-  {
-    name: 'Fake Name',
-    role: 'Copywriter',
-    imageUrl: 'https://i.pravatar.cc/1024?img=11',
-    twitterUrl: '#',
-    linkedinUrl: '#',
+    name: `আল-আমিন শাহেদ সুমন`,
+    role: `কোর্স কোর্ডিনেটর এবং কো-মেইনটেইনার`,
+    position: `সফটওয়্যার ইঞ্জিনিয়ার`,
+    image: imageOfShahed,
+    githubUrl: 'https://github.com/alaminsahed',
+    linkedinUrl: 'https://www.linkedin.com/in/md-al-amin-sahed/',
+    facebookUrl: 'https://www.facebook.com/sahed.sumon.2',
   },
 ];
+
+const SocialLinks = ({
+  githubUrl = '#',
+  linkedInUrl = '#',
+  facebookUrl = '#',
+}) => {
+  const socialLinks = [
+    { key: 'github', title: 'Github', url: githubUrl, icon: FaGithubSquare },
+    { key: 'linkedIn', title: 'LinkedIn', url: linkedInUrl, icon: FaLinkedin },
+    {
+      key: 'facebook',
+      title: 'Facebook',
+      url: facebookUrl,
+      icon: FaFacebookSquare,
+    },
+  ];
+
+  return (
+    <ul role="list" className="flex justify-center space-x-5">
+      {socialLinks.map((socialLink) => (
+        <li key={socialLink.key}>
+          <a
+            title={socialLink.title}
+            className="text-gray-400 hover:text-gray-500"
+            href={socialLink.url}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="sr-only">{socialLink.title}</span>
+            <socialLink.icon className="h-5 w-5 transform text-slate-400 duration-200 ease-in-out hover:scale-110 hover:text-slate-600 dark:hover:text-slate-300" />
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export const Team = () => {
   return (
@@ -32,68 +71,46 @@ export const Team = () => {
         <div className="space-y-12">
           <div className="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
             <h2 className="text-3xl font-extrabold tracking-tight dark:text-white sm:text-4xl">
-              Meet our team
+              কোডালয় সদস্য পরিচিতি
             </h2>
             <p className="text-xl text-gray-500 dark:text-slate-400">
-              Ornare sagittis, suspendisse in hendrerit quis. Sed dui aliquet
-              lectus sit pretium egestas vel mattis neque.
+              মাতৃভাষা বাংলায় আধুনিক প্রোগ্রামিং ও ওয়েব ডেভেলপমেন্ট শেখার
+              পূর্ণাঙ্গ গাইডলাইন সকলের কাছে বিনামূল্যে পৌঁছে দেওয়ার লক্ষ্যে
+              যারা অবদান রাখছেন
             </p>
           </div>
           <ul
             role="list"
-            className="mx-auto space-y-16 sm:grid sm:grid-cols-2 sm:gap-16 sm:space-y-0 lg:max-w-5xl lg:grid-cols-3"
+            className="mx-auto space-y-16 sm:grid sm:grid-cols-2 sm:gap-16 sm:space-y-0 lg:max-w-5xl"
           >
-            {people.map((person) => (
-              <li key={person.name}>
+            {teamMembers.map((member) => (
+              <li key={member.name}>
                 <div className="space-y-6">
-                  <img
-                    className="mx-auto h-40 w-40 rounded-full grayscale xl:h-56 xl:w-56"
-                    src={person.imageUrl}
-                    alt=""
-                  />
-                  <div className="space-y-2">
-                    <div className="space-y-1 text-lg font-medium leading-6 dark:text-white">
-                      <h3>{person.name}</h3>
-                      <p className="text-indigo-600">{person.role}</p>
+                  <div className="relative mx-auto h-40 w-40 overflow-hidden rounded-full border border-slate-200 grayscale dark:border-slate-900 xl:h-56 xl:w-56">
+                    <Image
+                      src={member.image}
+                      alt={member.name}
+                      layout="fill"
+                      placeholder="blur"
+                    />
+                  </div>
+                  <div className="space-y-3">
+                    <div className="space-y-3">
+                      <h3 className="text-2xl font-semibold leading-10 text-slate-800 dark:text-slate-100">
+                        {member.name}
+                      </h3>
+                      <p className="font-medium text-indigo-600 dark:text-indigo-400">
+                        {member.role}
+                      </p>
+                      <p className="text-sm font-normal text-slate-700 dark:text-slate-300">
+                        {member.position}
+                      </p>
                     </div>
-                    <ul role="list" className="flex justify-center space-x-5">
-                      <li>
-                        <a
-                          href={person.twitterUrl}
-                          className="text-gray-400 hover:text-gray-500"
-                        >
-                          <span className="sr-only">Twitter</span>
-                          <svg
-                            className="h-5 w-5"
-                            aria-hidden="true"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                          >
-                            <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                          </svg>
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href={person.linkedinUrl}
-                          className="text-gray-400 hover:text-gray-500"
-                        >
-                          <span className="sr-only">LinkedIn</span>
-                          <svg
-                            className="h-5 w-5"
-                            aria-hidden="true"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                        </a>
-                      </li>
-                    </ul>
+                    <SocialLinks
+                      githubUrl={member.githubUrl}
+                      linkedInUrl={member.linkedinUrl}
+                      facebookUrl={member.facebookUrl}
+                    />
                   </div>
                 </div>
               </li>
