5f7812990e088f7985b8f67bdffe8e9e08117935
Mukitul Islam Mukit <hellomukit@gmail.com>
2022-11-27 14:45:05 +0600

    style: update highlighted feature layout

--- src/components/home/HighlightedFeatures.tsx
+++ src/components/home/HighlightedFeatures.tsx
@@ -1,46 +1,48 @@
-import Image from 'next/image';
 import { BoltIcon, CommandLineIcon } from '@heroicons/react/24/outline';
-import codeEditorImage from '../../images/home/codealoy-live-editor.svg';
-import codeEditorImageBlurDataUrl from '../../images/home/codealoy-live-editor.svg.blur';
+import Image from 'next/image';
+import Link from 'next/link';
+
 import learnFasterImage from '../../images/home/codealoy-learn-faster.svg';
 import learnFasterImageBlurDataUrl from '../../images/home/codealoy-learn-faster.svg.blur';
+import codeEditorImage from '../../images/home/codealoy-live-editor.svg';
+import codeEditorImageBlurDataUrl from '../../images/home/codealoy-live-editor.svg.blur';
 
 export const HighlightedFeatures = () => {
   return (
-    <div className="relative overflow-hidden bg-white pt-16 pb-32 dark:border-t dark:border-slate-100/5 dark:bg-slate-900">
+    <div className="relative overflow-hidden bg-white py-32 dark:border-t dark:border-slate-100/5 dark:bg-slate-900">
       <div className="relative">
         <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2 lg:gap-24 lg:px-8">
-          <div className="mx-auto max-w-xl px-4 sm:px-6 lg:mx-0 lg:max-w-none lg:px-0 lg:pt-24 lg:pb-28">
+          <div className="mx-auto max-w-xl px-4 sm:px-6 lg:mx-0 lg:max-w-none lg:px-0 lg:py-20">
             <div>
-              <div>
-                <span className="flex h-12 w-12 items-center justify-center rounded-md bg-indigo-600">
-                  <CommandLineIcon
-                    className="h-6 w-6 text-white"
-                    aria-hidden="true"
-                  />
-                </span>
+              <div className="flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500">
+                <CommandLineIcon
+                  className="h-6 w-6 text-white"
+                  aria-hidden="true"
+                />
               </div>
-              <div className="mt-6">
+              <div className="mt-8">
                 <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">
                   লাইভ কোডিং এডিটর
                 </h2>
                 <p className="mt-4 text-lg text-gray-500">
-                  শুধুমাত্র সাঁতার দেখে যেমন সাঁতার শেখা যায় না, তেমনি কোডিং বা
-                  প্রোগ্রামিংও শুধু দেখে দেখে শেখা সম্ভব নয়। পরিচিত একটি প্রবাদ
-                  আছে, &quot;গাইতে গাইতে গায়েন&quot;। তাই শেখার পাশাপাশি
-                  প্রাকটিস করাটা বেশি গুরুত্বপূর্ণ। কোডালয় প্ল্যাটফর্মে রয়েছে
-                  লাইভ কোডিং এডিটর ফিচার, যেটা যেকোনো বিষয় শেখার সাথে সাথেই
-                  প্রাকটিস করার সুযোগ দিবে। আর লাইভ প্রাকটিস করার সুবিধা থাকায়
-                  শেখার সেটার উপযুক্ত ব্যবহার সম্পর্কে পাকাপোক্ত ধারণা পাওয়া
-                  যাবে। তাছাড়া যেকোন কনফিউশনের সমাধান করা যাবে।
+                  শুধুমাত্র সাঁতার দেখে দেখে যেমন সাঁতার শেখা যায় না, তেমনি
+                  কোডিং বা প্রোগ্রামিংও শুধু দেখে দেখে শেখা সম্ভব নয়। পরিচিত
+                  একটি প্রবাদ আছে, &quot;গাইতে গাইতে গায়েন&quot;। শেখার
+                  পাশাপাশি তাই প্রাকটিস করাটাই বেশি গুরুত্বপূর্ণ।
+                  <br />
+                  <br />
+                  কোডালয় প্ল্যাটফর্মে রয়েছে লাইভ কোডিং এডিটর ফিচার, যেটা
+                  যেকোনো টপিক শেখার সাথে সাথেই প্রাকটিস করার সুযোগ দিবে। আর লাইভ
+                  প্রাকটিস করতে পারায় শিক্ষণীয় বিষয়টির উপযুক্ত ব্যবহার
+                  সম্পর্কে পাকাপোক্ত ধারণা পাওয়া যাবে। তাছাড়া যেকোন কনফিউশনের
+                  থাকলে সেগুলোর তাৎক্ষণিক সমাধান করা সম্ভব হবে।
                 </p>
-                <div className="mt-6">
-                  <a
-                    href="#"
-                    className="inline-flex rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
-                  >
-                    Get started
-                  </a>
+                <div className="mt-8">
+                  <Link href="/courses">
+                    <a className="inline-flex rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 dark:hover:bg-indigo-500">
+                      শেখা শুরু করুন
+                    </a>
+                  </Link>
                 </div>
               </div>
             </div>
@@ -61,16 +63,16 @@ export const HighlightedFeatures = () => {
         </div>
       </div>
 
-      <div className="mt-32">
+      <div className="mt-36">
         <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2 lg:gap-24 lg:px-8">
-          <div className="mx-auto max-w-xl px-4 sm:px-6 lg:col-start-2 lg:mx-0 lg:max-w-none lg:px-0 lg:pt-28 lg:pb-32">
+          <div className="mx-auto max-w-xl px-4 sm:px-6 lg:col-start-2 lg:mx-0 lg:max-w-none lg:px-0 lg:py-20">
             <div>
               <div>
-                <span className="flex h-12 w-12 items-center justify-center rounded-md bg-indigo-600">
+                <span className="flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500">
                   <BoltIcon className="h-6 w-6 text-white" aria-hidden="true" />
                 </span>
               </div>
-              <div className="mt-6">
+              <div className="mt-8">
                 <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">
                   দ্বিগুণ গতিতে শেখার সুযোগ
                 </h2>
@@ -80,16 +82,18 @@ export const HighlightedFeatures = () => {
                   সাপেক্ষ। সাধারণত ভিডিও টিউটোরিয়ালগুলোতে গড়ে ১৫০ শব্দ/মিনিট
                   গতিতে কথা বলে, যেখানে আমরা গড়ে ~৩০০ শব্দ/মিনিট এর বেশি গতিতে
                   পড়তে পারি। তাছাড়া বাংলা ভাষাতে মানসম্পন্ন টিউটোরিয়ালের
-                  রয়েছে অভাব। একারণে আমাদের কোর্সগুলোতে লিখিত কনটেন্টের উপর জোর
-                  দেওয়া হয়েছে, যেটা দ্রুত লার্নিং এ সহায়তা করবে।
+                  রয়েছে অভাব।
+                  <br />
+                  <br />
+                  একারণে আমাদের কোর্সগুলোতে লিখিত কনটেন্টের উপর বেশি জোর দেওয়া
+                  হয়েছে, যেটা কমপক্ষে দ্বিগুণ গতিতে দ্রুত শেখায় সহায়তা করবে।
                 </p>
-                <div className="mt-6">
-                  <a
-                    href="#"
-                    className="inline-flex rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
-                  >
-                    শেখা শুরু করুন
-                  </a>
+                <div className="mt-8">
+                  <Link href="/courses">
+                    <a className="inline-flex rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 dark:hover:bg-indigo-500">
+                      শেখা শুরু করুন
+                    </a>
+                  </Link>
                 </div>
               </div>
             </div>
