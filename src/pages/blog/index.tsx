b583afb0b94ad82bef26b6f2ac55036fc6e4d5e6
Mukitul Islam Mukit <hellomukit@gmail.com>
2022-12-01 22:52:10 +0600

    docs: update blog page content

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -5,11 +5,10 @@ const BlogPage = () => {
     <div className="relative mx-auto mb-20 min-h-screen max-w-8xl px-4 sm:px-6 lg:px-8 xl:px-12">
       <div className="py-16 px-4 text-center sm:px-6 lg:px-8">
         <h1 className="text-4xl font-extrabold tracking-tight text-slate-700 dark:text-slate-300">
-          Blog Posts
+          ব্লগের পাতা
         </h1>
         <p className="mx-auto mt-4 max-w-xl text-base text-slate-500">
-          Want to learn something new? Don&apos;t get rid of anything, just put
-          it in really really nice looking containers.
+          কোডালয়ের সর্বশেষ ব্লগ পোস্টগুলো
         </p>
       </div>
       <div>
