2291a7b4eb93ca3551892296711b186f17117a2d
alaminsahed <alaminsahed97@gmail.com>
2022-12-07 01:09:20 +0600

    fix: fix index page blog bg color

--- src/components/home/LatestBlogPosts.tsx
+++ src/components/home/LatestBlogPosts.tsx
@@ -5,7 +5,7 @@ export const LatestBlogPosts = () => {
   return (
     <div className="relative bg-gray-50 px-4 pt-16 pb-20 dark:border-t dark:border-slate-100/5 dark:bg-slate-900 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28">
       <div className="absolute inset-0">
-        <div className="h-1/3 bg-white dark:bg-slate-900 sm:h-2/3" />
+        <div className="h-1/3 bg-gray-50 dark:bg-slate-900 sm:h-2/3" />
       </div>
       <div className="relative mx-auto max-w-7xl">
         <div className="text-center">
