cd2f9d34d05e35ba4e11747201e12771383ada23
Mukitul Islam Mukit <hellomukit@gmail.com>
2022-12-09 00:24:42 +0600

    refactor: create hero component for home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,14 @@
-import clsx from 'clsx';
 import Head from 'next/head';
-import Link from 'next/link';
-import styles from '../styles/index.module.css';
 
 import { CTA } from '../components/home/CTA';
 // import { FAQ } from '../components/home/FAQ';
+import { Divider } from '../components/Divider';
 import { FeatureGrid } from '../components/home/FeatureGrid';
-import { LatestBlogPosts } from '../components/home/LatestBlogPosts';
 import { HighlightedFeatures } from '../components/home/HighlightedFeatures';
+import { LatestBlogPosts } from '../components/home/LatestBlogPosts';
 import { Team } from '../components/home/Team';
 import { SITE_TITLE } from '../constants/site-settings';
-import Divider from 'src/components/Divider';
+import { Hero } from '../components/home/Hero';
 
 const IndexPage = () => {
   return (
@@ -20,48 +18,25 @@ const IndexPage = () => {
         <meta key="og:title" property="og:title" content={SITE_TITLE} />
         <title>{SITE_TITLE}</title>
       </Head>
-      <div
-        className={clsx(
-          '-mt-[76px] flex h-screen items-center justify-center space-y-20 overflow-hidden sm:space-y-32 md:space-y-40',
-          styles.beams,
-        )}
-      >
-        <div className="relative mx-auto max-w-5xl">
-          <h1 className="text-center text-4xl font-extrabold tracking-tight text-slate-900 dark:text-white sm:text-5xl lg:text-6xl">
-            কোড + বিদ্যালয় = কোডালয়
-          </h1>
-          <p className="mx-auto mt-6 max-w-3xl text-center text-lg text-slate-600 dark:text-slate-400">
-            মাতৃভাষা বাংলায় প্রোগ্রামিং এবং ওয়েব ডেভেলপমেন্ট শেখার
-            <code className="font-mono font-medium text-sky-500 dark:text-sky-400">
-              {' '}
-              সম্পূর্ণ ফ্রি
-            </code>
-            , এবং{' '}
-            <code className="font-mono font-medium text-sky-500 dark:text-sky-400">
-              ওপেনসোর্স
-            </code>{' '}
-            প্লাটফর্ম
-          </p>
-          <div className="mt-6 flex justify-center space-x-6 text-sm sm:mt-10">
-            <Link href="/courses">
-              <a className="dark:highlight-white/20 flex h-12 w-full items-center justify-center rounded-lg bg-indigo-600 px-6 font-semibold text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 dark:hover:bg-indigo-500 sm:w-auto">
-                শেখা শুরু করুন
-              </a>
-            </Link>
-          </div>
-        </div>
-      </div>
+
+      <Hero />
 
       <FeatureGrid />
+
       <Divider />
+
       <HighlightedFeatures />
 
       {/* <LatestCourses /> */}
 
       {/* <FAQ /> */}
 
-      <LatestBlogPosts />
       <Divider />
+
+      <LatestBlogPosts />
+
+      <Divider />
+
       <Team />
 
       <CTA />
