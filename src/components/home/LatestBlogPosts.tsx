2cdc2b7d8ab575caff7c1d3030b7d69e61f33141
Mukitul Islam Mukit <hellomukit@gmail.com>
2022-12-09 00:27:04 +0600

    style: fix blog section bg color

--- src/components/home/LatestBlogPosts.tsx
+++ src/components/home/LatestBlogPosts.tsx
@@ -3,9 +3,9 @@ import { BlogPosts } from '../BlogPosts';
 
 export const LatestBlogPosts = () => {
   return (
-    <div className="relative bg-slate-50 px-4 pt-16 pb-20 dark:bg-slate-900 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28">
+    <div className="relative bg-white px-4 pt-16 pb-20 dark:bg-slate-900 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28">
       <div className="absolute inset-0">
-        <div className="h-1/3 bg-slate-50 dark:bg-slate-900 sm:h-2/3" />
+        <div className="h-1/3 bg-white dark:bg-slate-900 sm:h-2/3" />
       </div>
       <div className="relative mx-auto max-w-7xl">
         <div className="text-center">
