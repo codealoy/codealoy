b289b913cd3d036ed5577e3ac660697064e6635d
Mukitul Islam Mukit <hellomukit@gmail.com>
2022-12-12 01:55:51 +0600

    chore: remove unnecessary import

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,6 @@
 import dayjs from 'dayjs';
 import Link from 'next/link';
 import React from 'react';
-import { useRouter } from 'next/router';
 
 import { HiOutlineUserGroup } from 'react-icons/hi';
 
