aa66cd479ed18d032e280f84951b332f4ffd9423
Mukitul Islam Mukit <hellomukit@gmail.com>
2022-12-12 01:26:55 +0600

    chore: add @tailwindcss/line-clamp plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -31,5 +31,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+  ],
 };
