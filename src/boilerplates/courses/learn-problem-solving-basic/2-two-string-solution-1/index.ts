2feb92d8588563ca4c4f6121fef2829cfa37e96b
Mukitul Islam Mukit <hellomukit@gmail.com>
2023-03-08 01:45:59 +0600

    feat: add test cases for module 2 solution 1

---
+++ src/boilerplates/courses/learn-problem-solving-basic/2-two-string-solution-1/index.ts
@@ -0,0 +1,28 @@
+/**
+ * @hidden
+ */
+
+import { SandpackProviderProps } from '@codesandbox/sandpack-react';
+
+import dynamicTests from './dynamic.test.js.json';
+
+import { generateDynamicTestsContent } from '../../../../utils/generateDynamicTestsContent';
+import { CodeEditorTestList } from '../../../../types/code-editor-tests';
+
+// @ts-ignore
+import functionCode from './function.js.txt';
+
+export const boilerplate2TwoStringSolution1: SandpackProviderProps = {
+  files: {
+    '/function.js': {
+      code: functionCode,
+    },
+    '/dynamic.test.js': {
+      code: generateDynamicTestsContent(dynamicTests as CodeEditorTestList),
+      readOnly: true,
+      hidden: true,
+    },
+  },
+  options: {},
+  customSetup: {},
+};
--- src/boilerplates/courses/learn-problem-solving-basic/index.ts
+++ src/boilerplates/courses/learn-problem-solving-basic/index.ts
@@ -1,5 +1,7 @@
 import { boilerplate2IsUnique } from './2-is-unique';
+import { boilerplate2TwoStringSolution1 } from './2-two-string-solution-1';
 
 export const learnProblemSolvingBasic = {
   '2-is-unique': boilerplate2IsUnique,
+  '2-two-string-solution-1': boilerplate2TwoStringSolution1,
 };
