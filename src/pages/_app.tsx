0ff26f1306bc517a81434a3c3aeff003eb8aad5a
Mukitul Islam Mukit <hellomukit@gmail.com>
2023-03-12 23:08:41 +0600

    refactor: rename preve to previousValue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -85,7 +85,7 @@ const MyApp: AppType<MyAppProps> = ({
   };
 
   const hideAnnouncement = () => {
-    setShowAnnouncement((preve) => !preve);
+    setShowAnnouncement((previousValue) => !previousValue);
   };
 
   return (
