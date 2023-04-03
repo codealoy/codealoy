5e0987062773c9b3852be704a78e04f88dfb69ee
md-habib <mdhabibbhuyia@gmail.com>
2023-04-03 14:44:32 +0600

    fix: active page menu style

--- src/components/PrimaryNavItems.tsx
+++ src/components/PrimaryNavItems.tsx
@@ -1,17 +1,25 @@
 import Link from 'next/link';
 import { getNavigationItems } from '../utils/getNavigationItems';
 
+import { useState } from 'react';
+
 const navigationItems = getNavigationItems({
   scope: 'PRIMARY',
 });
 
 export const PrimaryNavItems = () => {
+  const [active, setActive] = useState(0);
   return (
     <>
-      {navigationItems.map((navigationItem) => (
+      {navigationItems.map((navigationItem, i) => (
         <li key={navigationItem.title}>
           <Link href={navigationItem.href!}>
-            <span className="hover:text-sky-500 dark:hover:text-sky-400">
+            <span
+              onClick={() => setActive(i)}
+              className={`${
+                active === i ? 'text-sky-500' : 'none'
+              }  hover:text-sky-500 dark:hover:text-sky-400`}
+            >
               {navigationItem.title}
             </span>
           </Link>
