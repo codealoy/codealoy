f87ab733c0b21290172e6f6baf22b6b7f1fe5633
Mukitul Islam Mukit <hellomukit@gmail.com>
2023-04-06 01:50:10 +0600

    feat: add example api with db client

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,10 +1,10 @@
 import { z } from 'zod';
-
 import {
   createTRPCRouter,
   publicProcedure,
   protectedProcedure,
 } from '~/server/api/trpc';
+import { db } from '~/server/database';
 
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
@@ -14,6 +14,10 @@ export const exampleRouter = createTRPCRouter({
         greeting: input?.text ? `Hello ${input?.text}` : `Hello world`,
       };
     }),
+
+  getMigrationList: publicProcedure.query(async () => {
+    return await db.query.select().from(db.models.schemaMigration).limit(10);
+  }),
 
   getSecretMessage: protectedProcedure.query(() => {
     return 'you can now see this secret message!';
