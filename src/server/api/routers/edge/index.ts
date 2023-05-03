e4340a9ac2a93ae4ceb7c0af045c9ca33861e4bb
alaminsahed <alaminsahed97@gmail.com>
2023-05-03 09:25:14 +0600

    fix: remove tag route

--- src/server/api/routers/edge/index.ts
+++ src/server/api/routers/edge/index.ts
@@ -1,9 +1,7 @@
 import { exampleEdgeRouter } from '~/server/api/routers/edge/example';
 import { submitSolutionEdgeRouter } from '~/server/api/routers/edge/submitSolution';
-import { submitTagsEdgeRouter } from '~/server/api/routers/edge/submitTags';
 
 export const edgeRouters = {
   example: exampleEdgeRouter,
   solution: submitSolutionEdgeRouter,
-  submitTags: submitTagsEdgeRouter,
 };
