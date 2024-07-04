eaddd20ebc46ed0b8247398940d637dcc54d0ae5
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-07-04 21:16:11 +0600

    refactor: create two separate utils functions

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,9 +1,6 @@
-import { customAlphabet } from 'nanoid';
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export const nanoid = customAlphabet('abcdefghijklmnopqrstuvwxyz0123456789');
