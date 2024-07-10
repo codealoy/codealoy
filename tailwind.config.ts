805ea411ad7a4879323ac911854dfebb62b7d71b
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-07-11 00:52:41 +0600

    chore: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,34 @@
 import { type Config } from 'tailwindcss';
 import defaultTheme from 'tailwindcss/defaultTheme';
+import typographyPlugin from '@tailwindcss/typography';
+import tailwindAnimate from 'tailwindcss-animate';
+
+import typographyStyles from './typography';
 
 export default {
-  darkMode: ['class'],
-  content: ['src/app/**/*.{ts,tsx}', 'src/components/**/*.{ts,tsx}'],
+  darkMode: 'selector',
+  content: ['./src/**/*.{js,mjs,jsx,ts,tsx,mdx}'],
   theme: {
     container: {
       center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px',
-      },
     },
     fontSize: {
-      xs: ['0.75rem', { lineHeight: '1rem' }],
+      '2xs': ['0.75rem', { lineHeight: '1.25rem' }],
+      xs: ['0.8125rem', { lineHeight: '1.5rem' }],
       sm: ['0.875rem', { lineHeight: '1.5rem' }],
-      base: ['1rem', { lineHeight: '2rem' }],
+      base: ['1rem', { lineHeight: '1.75rem' }],
       lg: ['1.125rem', { lineHeight: '1.75rem' }],
-      xl: ['1.25rem', { lineHeight: '2rem' }],
-      '2xl': ['1.5rem', { lineHeight: '2.5rem' }],
-      '3xl': ['2rem', { lineHeight: '2.5rem' }],
-      '4xl': ['2.5rem', { lineHeight: '3rem' }],
-      '5xl': ['3rem', { lineHeight: '3.5rem' }],
+      xl: ['1.25rem', { lineHeight: '1.75rem' }],
+      '2xl': ['1.5rem', { lineHeight: '2rem' }],
+      '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+      '4xl': ['2.25rem', { lineHeight: '2.5rem' }],
+      '5xl': ['3rem', { lineHeight: '1' }],
       '6xl': ['3.75rem', { lineHeight: '1' }],
       '7xl': ['4.5rem', { lineHeight: '1' }],
       '8xl': ['6rem', { lineHeight: '1' }],
       '9xl': ['8rem', { lineHeight: '1' }],
     },
+    typography: typographyStyles,
     extend: {
       fontFamily: {
         sans: ['var(--font-bengali)', ...defaultTheme.fontFamily.sans],
@@ -41,6 +43,9 @@ export default {
         primary: {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))',
+          // DEFAULT: '#B1E346',
+          100: '#F3F8E8',
+          200: '#C4F241',
         },
         secondary: {
           DEFAULT: 'hsl(var(--secondary))',
@@ -66,11 +71,51 @@ export default {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
         },
+        dark: {
+          DEFAULT: '#131410',
+          100: '#141410',
+          200: '#212220',
+          300: '#191A17',
+          gradient: '#191917',
+        },
+        gray: {
+          DEFAULT: '#F6F8F1',
+          100: '#DCE0D3',
+          200: '#D9E0C5',
+          50: '#F7F7F7',
+          darkGradient: 'rgba(217, 224, 197, .07)',
+        },
+        borderColor: {
+          DEFAULT: '#EDF0E6',
+          dark: '#373935',
+        },
+        paragraph: {
+          DEFAULT: '#18181B',
+          light: '#5D6167',
+        },
+        rating: {
+          DEFAULT: '#FFC107',
+        },
       },
       borderRadius: {
         lg: `var(--radius)`,
         md: `calc(var(--radius) - 2px)`,
         sm: 'calc(var(--radius) - 4px)',
+      },
+      boxShadow: {
+        glow: '0 0 4px rgb(0 0 0 / 0.1)',
+      },
+      maxWidth: {
+        lg: '33rem',
+        '2xl': '40rem',
+        '3xl': '50rem',
+        '5xl': '66rem',
+      },
+      opacity: {
+        1: '0.01',
+        2.5: '0.025',
+        7.5: '0.075',
+        15: '0.15',
       },
       keyframes: {
         'accordion-down': {
@@ -88,5 +133,5 @@ export default {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography'), require('tailwindcss-animate')],
+  plugins: [typographyPlugin, tailwindAnimate],
 } satisfies Config;
