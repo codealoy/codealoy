0bbf4f3a58d859b8fa815fda3cbbb90a18423d23
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-07-31 02:25:41 +0600

    feat: add navigation config

---
+++ src/config/navigations/primary.ts
@@ -0,0 +1,18 @@
+export const PRIMARY_NAV_ITEMS = [
+  {
+    title: 'হোম',
+    href: '/',
+  },
+  {
+    title: 'কোর্সসমূহ',
+    href: '/courses',
+  },
+  {
+    title: 'ব্লগ',
+    href: '/blog',
+  },
+  {
+    title: 'লগইন',
+    href: '/login',
+  },
+];
