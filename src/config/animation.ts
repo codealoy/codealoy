b8f014eb3a04995558e7442b92362680421cd39c
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-07-31 02:27:10 +0600

    feat: add animation config

---
+++ src/config/animation.ts
@@ -0,0 +1,82 @@
+export const fadeUpAnimation = {
+  initial: { opacity: 0, y: 100 },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
+export const fadeFromLeftAnimation = {
+  initial: { opacity: 0, x: 100 },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+      delay: 0.2,
+    },
+  },
+};
+export const fadeFromLeftAnimation2 = {
+  initial: { opacity: 0, x: 100 },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+      delay: 0.4,
+    },
+  },
+};
+export const fadeFromLeftAnimation3 = {
+  initial: { opacity: 0, x: 100 },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+      delay: 0.6,
+    },
+  },
+};
+export const fadeFromRightAnimation = {
+  initial: { opacity: 0, x: -100 },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+      delay: 0.2,
+    },
+  },
+  viewPort: { once: true },
+};
+
+export const fadeFromRightAnimation2 = {
+  initial: { opacity: 0, x: -100 },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+      delay: 0.4,
+    },
+  },
+  viewPort: { once: true },
+};
+
+export const fadeFromRightAnimation3 = {
+  initial: { opacity: 0, x: -100 },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+      delay: 0.6,
+    },
+  },
+  viewPort: { once: true },
+};
