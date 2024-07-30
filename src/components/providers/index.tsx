83c7f2a78944822c4ee1701b935e2269704b0d46
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-07-31 02:22:25 +0600

    feat: add root providers component

---
+++ src/components/providers/index.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { QueryClientProvider } from '@/components/providers/QueryClientProvider';
+import { ThemeProvider } from '@/components/providers/ThemeProvider';
+
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+export const Providers: React.FC<ProvidersProps> = ({ children }) => {
+  return (
+    <ThemeProvider>
+      <QueryClientProvider>{children}</QueryClientProvider>
+    </ThemeProvider>
+  );
+};
