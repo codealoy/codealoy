82a9af5dda4a16f01c392a199beb5ae8e2275748
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-07-31 02:26:52 +0600

    feat: update button ui component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,15 +5,15 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-full text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        default: 'bg-primary text-primary-foreground hover:bg-primary/80',
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
-          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
+          'bg-green-500/20 text-green-500 ring-1 ring-inset ring-green-500/20 hover:text-green-600 hover:ring-green-600 dark:hover:text-green-400 dark:hover:ring-green-400',
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
@@ -21,8 +21,9 @@ const buttonVariants = cva(
       },
       size: {
         default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
+        xs: 'rounded-full px-3 py-1',
+        sm: 'h-9 rounded-full px-3',
+        lg: 'h-11 rounded-full px-8',
         icon: 'h-10 w-10',
       },
     },
