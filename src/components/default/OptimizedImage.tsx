14940c984e8c0c7a74fd5a3592050ed68a2fcbfb
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-07-31 22:10:19 +0600

    feat: add optimized image component

---
+++ src/components/default/OptimizedImage.tsx
@@ -0,0 +1,21 @@
+import Image, { ImageProps } from 'next/image';
+
+import { defaultImageBlurDataUrl } from '@/config/defaultImageBlur';
+
+export const OptimizedImage = ({
+  placeholder = 'blur',
+  blurDataURL = defaultImageBlurDataUrl,
+  loading = 'lazy',
+  alt = '',
+  ...restProps
+}: ImageProps) => {
+  return (
+    <Image
+      placeholder={placeholder}
+      blurDataURL={blurDataURL}
+      loading={loading}
+      alt={alt}
+      {...restProps}
+    />
+  );
+};
