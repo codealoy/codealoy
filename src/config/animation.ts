4e55605897298f43660e4e29f0faa584e5d88b56
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-07-31 22:37:00 +0600

    fix: animation direction & clean up

--- src/config/animation.ts
+++ src/config/animation.ts
@@ -10,73 +10,25 @@ export const fadeUpAnimation = {
 };
 
 export const fadeFromLeftAnimation = {
-  initial: { opacity: 0, x: 100 },
+  initial: { opacity: 0, x: -100 },
   animate: {
     opacity: 1,
     x: 0,
     transition: {
       duration: 0.5,
-      delay: 0.2,
+      delay: 0.5,
     },
   },
 };
-export const fadeFromLeftAnimation2 = {
-  initial: { opacity: 0, x: 100 },
-  animate: {
-    opacity: 1,
-    x: 0,
-    transition: {
-      duration: 0.5,
-      delay: 0.4,
-    },
-  },
-};
-export const fadeFromLeftAnimation3 = {
-  initial: { opacity: 0, x: 100 },
-  animate: {
-    opacity: 1,
-    x: 0,
-    transition: {
-      duration: 0.5,
-      delay: 0.6,
-    },
-  },
-};
+
 export const fadeFromRightAnimation = {
-  initial: { opacity: 0, x: -100 },
+  initial: { opacity: 0, x: 100 },
   animate: {
     opacity: 1,
     x: 0,
     transition: {
       duration: 0.5,
-      delay: 0.2,
+      delay: 0.5,
     },
   },
-  viewPort: { once: true },
-};
-
-export const fadeFromRightAnimation2 = {
-  initial: { opacity: 0, x: -100 },
-  animate: {
-    opacity: 1,
-    x: 0,
-    transition: {
-      duration: 0.5,
-      delay: 0.4,
-    },
-  },
-  viewPort: { once: true },
-};
-
-export const fadeFromRightAnimation3 = {
-  initial: { opacity: 0, x: -100 },
-  animate: {
-    opacity: 1,
-    x: 0,
-    transition: {
-      duration: 0.5,
-      delay: 0.6,
-    },
-  },
-  viewPort: { once: true },
 };
