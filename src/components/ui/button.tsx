dc60061e1d38dafdbc3d4b68a58c98b300f57e14
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-07-31 14:17:01 +0600

    style: update default button text color

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/80',
+        default: 'bg-primary text-black hover:bg-primary/80 hover:text-white',
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
