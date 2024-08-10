2358a95ed4fa9cd67b3a0e8d026fa65dc60affdb
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-10 13:20:10 +0600

    chore: update prettier config to sort imports

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,19 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  plugins: [
+    require.resolve('prettier-plugin-tailwindcss'),
+    require.resolve('@trivago/prettier-plugin-sort-imports'),
+  ],
+  importOrder: [
+    '<THIRD_PARTY_MODULES>',
+    '^@/lib/(.*)$',
+    '^@/components/(.*)$',
+    '^./(.*)$',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderGroupNamespaceSpecifiers: true,
+  importOrderCaseInsensitive: true,
   trailingComma: 'all',
   tabWidth: 2,
   semi: true,
