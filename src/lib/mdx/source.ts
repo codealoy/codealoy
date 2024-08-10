2155d7f530f6c3079eebea798ae4c4c03853f867
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-10 13:24:52 +0600

    chore: update mdx lib

--- src/lib/mdx/source.ts
+++ src/lib/mdx/source.ts
@@ -1,8 +1,8 @@
-import { loader } from 'fumadocs-core/source';
-import { allCourses, allMetas } from 'content-collections';
 import { createMDXSource } from '@fumadocs/content-collections';
+import { allCourses, allMetas } from 'content-collections';
+import { loader } from 'fumadocs-core/source';
 
-export const { getPage, getPages, pageTree } = loader({
+export const { getPage, getPages, pageTree, getLanguages } = loader({
   baseUrl: '/courses',
   source: createMDXSource(allCourses, allMetas),
 });
