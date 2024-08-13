1b26d1707331946a3346c9f28c475ee5efd409ae
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-13 16:48:22 +0600

    feat: make code block component dynamic

--- src/components/course/index.ts
+++ src/components/course/index.ts
@@ -1,4 +1,15 @@
-export * from './CodeBlock';
+import dynamic from 'next/dynamic';
+
 export * from './CourseHeader';
+export * from './CodeBlock';
 export * from './CourseContent';
 export * from './CourseNavbar';
+
+// Dynamic exports.
+// Learn more: https://nextjs.org/docs/app/building-your-application/optimizing/lazy-loading#importing-named-exports
+
+const DynamicCodeBlock = dynamic(() =>
+  import('./CodeBlock').then((mod) => mod.CodeBlock),
+);
+
+export { DynamicCodeBlock };
--- src/lib/mdx/mdxComponentMapper.ts
+++ src/lib/mdx/mdxComponentMapper.ts
@@ -1,8 +1,8 @@
 import Link from 'fumadocs-core/link';
 
-import { CodeBlock } from '@/components/course';
+import { DynamicCodeBlock } from '@/components/course';
 
 export const MDX_COMPONENT_MAP = {
   Link,
-  pre: CodeBlock,
+  pre: DynamicCodeBlock,
 };
