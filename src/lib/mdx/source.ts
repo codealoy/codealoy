67786f4cb6eabfa7ccf36c5a968ffdb80ac68555
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-14 00:50:03 +0600

    feat: add caching to content loader functions

--- src/lib/mdx/source.ts
+++ src/lib/mdx/source.ts
@@ -1,8 +1,18 @@
+import React from 'react';
 import { createMDXSource } from '@fumadocs/content-collections';
 import { allCourses, allMetas } from 'content-collections';
 import { loader } from 'fumadocs-core/source';
 
-export const { getPage, getPages, pageTree, getLanguages } = loader({
+const loaderOutput = loader({
   baseUrl: '/courses',
   source: createMDXSource(allCourses, allMetas),
 });
+
+const getPage = React.cache(loaderOutput.getPage);
+const getPages = React.cache(loaderOutput.getPages);
+const getLanguages = React.cache(loaderOutput.getLanguages);
+
+const pageTree = loaderOutput.pageTree;
+const files = loaderOutput.files;
+
+export { getPage, getPages, getLanguages, pageTree, files };
