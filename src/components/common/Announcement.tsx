b077139273b07b7adef779fd8f085ab6c65d950e
Mukitul Islam Mukit <hellomukit@gmail.com>
2025-11-07 21:11:51 +0600

    feat: add Announcement component for user notifications and GitHub support

---
+++ src/components/common/Announcement.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import React from 'react';
+import { HiOutlineArrowRight, HiX } from 'react-icons/hi';
+
+import { GITHUB_REPO_LINK } from '@/config/site';
+
+import { Button } from '@/components/ui/button';
+
+export default function Announcement() {
+  const [hideAnnouncement, setHideAnnouncement] = React.useState(false);
+
+  const handleHideAnnouncement = () => {
+    setHideAnnouncement(true);
+  };
+
+  return hideAnnouncement ? null : (
+    <div className="to-primary z-50 flex bg-linear-to-r from-[#3abff8] text-white">
+      <div className="mx-auto max-w-7xl py-2">
+        <div className="flex flex-wrap items-center justify-center space-x-2">
+          <div className="flex items-center space-x-1">
+            <p className="truncate text-sm font-medium md:text-base">
+              <span className="md:hidden">
+                কোডালয়{' '}
+                <a
+                  href={GITHUB_REPO_LINK}
+                  target="_blank"
+                  aria-label="Github Link"
+                  rel="noopener noreferrer"
+                >
+                  <span className="text-sm font-medium underline underline-offset-4 hover:text-black md:text-base">
+                    গিটহাব কোডবেস
+                  </span>
+                </a>{' '}
+                - এ ⭐ দিন।
+              </span>
+              <span className="hidden md:inline">
+                কোডালয় প্রজেক্টটি সম্পূর্ণ ফ্রি এবং ওপেনসোর্স। গিটহাব - এ ⭐
+                দিয়ে সাপোর্ট করুন{' '}
+                <HiOutlineArrowRight className="inline text-base" />{' '}
+                <a
+                  href={GITHUB_REPO_LINK}
+                  target="_blank"
+                  aria-label="Github Link"
+                  rel="noopener noreferrer"
+                >
+                  <span className="text-sm font-medium underline underline-offset-4 hover:text-black md:text-base">
+                    গিটহাব লিংক
+                  </span>
+                </a>
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="mr-3 flex shrink-0 items-center">
+        <Button
+          variant="icon"
+          size="icon-lg"
+          aria-label="Hide Announcement"
+          onClick={handleHideAnnouncement}
+        >
+          <HiX
+            className="focus:ring-primary h-5 w-5 scale-100 transition-all duration-200 hover:scale-125 focus:ring-1 focus:outline-none focus:ring-inset active:scale-90"
+            aria-hidden="true"
+          />
+        </Button>
+      </div>
+    </div>
+  );
+}
