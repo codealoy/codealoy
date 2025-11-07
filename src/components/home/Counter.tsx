7aaed0469209b70a8eaf11cb306a938d7923eba6
Mukitul Islam Mukit <hellomukit@gmail.com>
2025-11-07 23:48:12 +0600

    feat: add Counter component to display animated numerical data with responsive design

---
+++ src/components/home/Counter.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import React from 'react';
+import { useWhileInView } from '@/hooks/useWhileInView';
+import { motion } from 'framer-motion';
+
+import { fadeUpAnimation } from '@/config/animation';
+import { counterData } from '@/config/data';
+
+import { cn } from '@/lib/utils';
+
+export default function Counter() {
+  const ref = React.useRef<HTMLDivElement>(null!);
+  const controlAnimation = useWhileInView(ref);
+
+  return (
+    <motion.section
+      initial="initial"
+      animate={controlAnimation}
+      variants={fadeUpAnimation}
+      ref={ref}
+      className="relative z-10 mx-5 -mb-20"
+    >
+      <div className="border-primary/30 dark:border-primary/15 dark:bg-dark-light/80 mx-auto h-40 max-w-3xl rounded-3xl border bg-white/90 p-3 shadow-lg">
+        <div
+          className={cn(
+            'grid h-full grid-cols-3 items-center gap-y-10 rounded-2xl border border-dashed border-gray-300 dark:border-gray-600',
+            '[&>*:not(:last-child)]:border-primary [&>*:not(:last-child)]:border-r [&>*:not(:last-child)]:border-dashed',
+          )}
+          id="counter"
+        >
+          {counterData.map((item) => (
+            <div
+              className="flex flex-col items-center justify-center space-y-1"
+              key={item.id}
+            >
+              <p className="text-xs text-slate-600 dark:text-slate-400">
+                {item.subText}
+              </p>
+              <p className="text-primary text-5xl font-bold">
+                <span>{item.number}</span>
+                <span>+</span>
+              </p>
+              <h4 className="text-slate-700 dark:text-slate-300">
+                {item.text}
+              </h4>
+            </div>
+          ))}
+        </div>
+      </div>
+    </motion.section>
+  );
+}
