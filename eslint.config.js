7a276a5aa239e76806cf625bef69e2a4d739400e
Mukitul Islam Mukit <hellomukit@gmail.com>
2025-11-08 18:17:48 +0600

    chore: eslint ignore .astro generated files

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,9 @@ export default [
   // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   {
+    ignores: ['.astro/**'],
+  },
+  {
     rules: {
       // override/add rules settings here, such as:
       // "astro/no-set-html-directive": "error"
