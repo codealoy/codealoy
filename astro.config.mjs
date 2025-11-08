879d7e13e8307f3156d7faa0c9c3f655579ac0dc
Mukitul Islam Mukit <hellomukit@gmail.com>
2025-11-08 18:22:06 +0600

    chore: update site URL and set trailing slash configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import { defineConfig, envField } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.codealoy.com',
+  trailingSlash: 'never',
   env: {
     schema: {
       NODE_ENV: envField.string({
