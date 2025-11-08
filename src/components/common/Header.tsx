56cec79e27502c6a352e726f64b17f0cb075d71b
Mukitul Islam Mukit <hellomukit@gmail.com>
2025-11-08 13:59:26 +0600

    feat: implement Header component with responsive navigation and theme switching functionality

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,104 @@
+'use client';
+
+import React from 'react';
+import { HiOutlineMenuAlt2 } from 'react-icons/hi';
+import { HiOutlineXMark } from 'react-icons/hi2';
+
+import { PRIMARY_NAV_ITEMS } from '@/config/primaryNavigation';
+
+import { cn } from '@/lib/utils';
+
+import { Logo } from '@/components/common/Logo';
+import ThemeSwitcher from '@/components/common/ThemeSwitcher';
+import { Button } from '@/components/ui/button';
+
+export default function Header() {
+  const [menuState, setMenuState] = React.useState(false);
+  const [isScrolled, setIsScrolled] = React.useState(false);
+
+  React.useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+  return (
+    <header>
+      <nav
+        data-state={menuState && 'active'}
+        className="fixed z-20 w-full px-2"
+      >
+        {/* Desktop Navigation */}
+        <div
+          className={cn(
+            'bg-background/50 mx-auto mt-2 max-w-7xl rounded-xl px-6 backdrop-blur-md transition-all duration-300 lg:px-12',
+            isScrolled && 'border-primary/10 max-w-4xl border lg:px-5',
+          )}
+        >
+          <div className="relative flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4">
+            <div className="flex w-full justify-between lg:w-auto">
+              <a
+                href="/"
+                aria-label="home"
+                className="flex items-center space-x-2"
+              >
+                <Logo className="h-6" />
+              </a>
+
+              <button
+                onClick={() => setMenuState(!menuState)}
+                aria-label={menuState == true ? 'Close Menu' : 'Open Menu'}
+                className="relative z-20 -m-2.5 -mr-4 block cursor-pointer p-2.5 lg:hidden"
+              >
+                <HiOutlineMenuAlt2 className="m-auto size-5 duration-200 in-data-[state=active]:scale-0 in-data-[state=active]:rotate-180 in-data-[state=active]:opacity-0" />
+                <HiOutlineXMark className="absolute inset-0 m-auto size-5 scale-0 -rotate-180 opacity-0 duration-200 in-data-[state=active]:scale-100 in-data-[state=active]:rotate-0 in-data-[state=active]:opacity-100" />
+              </button>
+            </div>
+
+            <div className="absolute inset-0 m-auto hidden size-fit lg:block">
+              <ul className="flex gap-8 text-sm">
+                {PRIMARY_NAV_ITEMS.map((item, index) => (
+                  <li key={index}>
+                    <a
+                      href={item.href}
+                      className="text-muted-foreground hover:text-accent-foreground block duration-150"
+                    >
+                      <span>{item.title}</span>
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* Mobile Navigation */}
+            <div className="bg-background mb-6 hidden w-full flex-wrap items-center justify-end space-y-8 rounded-xl border p-6 shadow-xs in-data-[state=active]:block md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none lg:in-data-[state=active]:flex dark:shadow-none dark:lg:bg-transparent">
+              <div className="lg:hidden">
+                <ul className="space-y-6 text-base">
+                  {PRIMARY_NAV_ITEMS.map((item, index) => (
+                    <li key={index}>
+                      <a
+                        href={item.href}
+                        className="text-muted-foreground hover:text-accent-foreground block duration-150"
+                      >
+                        <span>{item.title}</span>
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit">
+                <ThemeSwitcher />
+                <Button asChild size="sm" className="inline-flex">
+                  <a href="#">
+                    <span>Get Started</span>
+                  </a>
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
