26f5046580536f2d6c44203b358738ebec782488
Mukitul Islam Mukit <hellomukit@gmail.com>
2025-11-08 14:50:03 +0600

    style: update BottomWavePattern component for improved color consistency and layout

--- src/components/patterns/BottomWavePattern.tsx
+++ src/components/patterns/BottomWavePattern.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export const BottomWavePattern = () => {
   return (
     <div
@@ -5,7 +7,7 @@ export const BottomWavePattern = () => {
       aria-hidden="true"
     >
       <div
-        className="from-complimentary to-primary w-72.1875rem aspect-1155/678 bg-linear-to-tr opacity-25"
+        className="to-primary from-primary/80 aspect-1155/678 w-6xl bg-linear-to-tr opacity-25"
         style={{
           clipPath:
             'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
