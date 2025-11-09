95707dea55548083ee41b1e453bbd6b4ea5e1168
Mukitul Islam Mukit <hellomukit@gmail.com>
2025-11-09 15:50:34 +0600

    fix: change trailingSlash configuration from 'never' to 'ignore'

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.codealoy.com',
-  trailingSlash: 'never',
+  trailingSlash: 'ignore',
   env: {
     schema: {
       NODE_ENV: envField.string({
